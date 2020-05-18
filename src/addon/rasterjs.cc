f9316b6a766876a6d6adb89a5c07ff2f850415b8
Dustin Long <me@dustmop.io>
2020-05-17 22:22:35 -0400

    Open SDL window and handle close event

--- src/addon/rasterjs.cc
+++ src/addon/rasterjs.cc
@@ -52,14 +52,51 @@ Napi::Value RasterJS::CallOne(const Napi::CallbackInfo& info) {
   return Napi::Number::New(env, 0);
 }
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 Napi::Value RasterJS::CallTwo(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
-  printf("two\n");
+
+  if (!sdl_initialized) {
+    return Napi::Number::New(env, -1);
+  }
+
+  // Create window
+  window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+  if(window == NULL) {
+    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+  }
   return Napi::Number::New(env, 0);
 }
 
 Napi::Value RasterJS::CallThree(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
-  printf("three\n");
+
+  if (!sdl_initialized || !window) {
+    return Napi::Number::New(env, -1);
+  }
+
+  //Get window surface
+  screenSurface = SDL_GetWindowSurface( window );
+
+  //Fill the surface white
+  SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+  //Update the surface
+  SDL_UpdateWindowSurface( window );
+
+  // A basic main loop to handle events
+  bool is_running = true;
+  SDL_Event event;
+  while (is_running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        is_running = false;
+      }
+    }
+    SDL_Delay(16);
+  }
+
   return Napi::Number::New(env, 0);
 }

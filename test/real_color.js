ab56b5b2a321616b92e52289461340c0e7fbd358
Dustin Long <me@dustmop.io>
2021-01-11 21:18:29 -0500

    Stubs for needed tests

---
+++ test/real_color.js
@@ -0,0 +1,9 @@
+var util = require('./util.js');
+var ra = require('../src/lib.js');
+
+describe('Real Color', function() {
+  it('real color drawing', function() {
+    console.log("TODO: Not Implemented");
+    // TODO: setRealColor, fillRealBackground
+  });
+});
---
+++ test/system_colors.js
@@ -0,0 +1,10 @@
+var util = require('./util.js');
+var ra = require('../src/lib.js');
+
+describe('System Colors', function() {
+  it('using system colors', function() {
+    console.log("TODO: Not Implemented");
+    // TODO: useSystemColors('nes'), useSystemColors(null)
+    // TODO: useSystemColors([0xff0000, 0x00ff00, 0x0000ff])
+  });
+});

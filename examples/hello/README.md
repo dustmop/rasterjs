7bf4b9bc4855fd2a1b8787ed62511a72f0824d3a
Dustin Long <me@dustmop.io>
2022-02-19 12:10:38 -0500

    Update the example app for improved clarity

--- examples/hello/README.md
+++ examples/hello/README.md
@@ -2,16 +2,12 @@
 
 Clone this repo using `git clone`
 
-Grab the [latest release](https://github.com/dustmop/rasterjs/releases), or build your own with `npm run build`
-
-Put the file `raster.min.js` into this directory
-
 `npm install -g simplehttpserver`
 
 `simplehttpserver` and open your browser to `http://localhost:8000`
 
 ## Development
 
-Optionally, you can create a development version of raster.js by running `npm run dev`. This will create `raster.dev.js`. Edit the file index.html to use this bundle instead.
+Edit `app.js` and refresh the page in your browser
 
-If you wish to rename the main file away from `app.js`, remember to also modify the including script tag in `index.html`.
+To switch to the minimized version of raster.js, run `npm run build` in the root of this repository, copy `dist/raster.min.js` here, and edit `index.html` to include that file instead of `raster.dev.js`.
--- examples/hello/app.js
+++ examples/hello/app.js
@@ -3,14 +3,19 @@ const ra = require('raster');
 ra.setSize(80, 40);
 ra.setZoom(8);
 
-ra.fillColor(45);
-ra.setColor(30);
+const BACK_COLOR = 45;
+const FRONT_COLOR = 38;
+const SHADE_COLOR = 30;
+const DROP_HEIGHT = 1;
+
+ra.fillColor(BACK_COLOR);
+ra.setColor(SHADE_COLOR);
 
 for (let j = 0; j < 2; j++) {
-  let sel = ra.select({x:4, y:6-j, w:76, h:32});
+  let sel = ra.select({x:4, y:6-j*DROP_HEIGHT, w:76, h:32});
 
   for (let offs = 0; offs < 39; offs += 38) {
-    sel.fillRect({x:  offs, y:0, w:4, h:24});
+    sel.fillRect({x:offs, y:0, w:4, h:24});
     sel.fillRect({x:10+offs, y:0, w:4, h:24});
     sel.drawCircle({x:18+offs, y:8, r:8, width:4});
   }
@@ -18,12 +23,12 @@ for (let j = 0; j < 2; j++) {
   sel.fillRect({x: 0, y:10, w:12, h:4});
   sel.fillRect({x:20, y:14, w:12, h:3});
 
-  ra.setColor(38);
+  ra.setColor(FRONT_COLOR);
 }
 
-ra.setColor(30);
+ra.setColor(SHADE_COLOR);
 ra.drawLine(32, 22, 37, 22);
-ra.setColor(45);
+ra.setColor(BACK_COLOR);
 ra.drawLine(32, 23, 37, 23);
 ra.drawDot(37, 24);
 
